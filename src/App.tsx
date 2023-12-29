70bb49f1b0fc622f04a93485e5b35b8ad2cc0654
Placid Rodrigues <placidrod@gmail.com>
2023-12-29 14:38:37 +1300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center p-20 bg-slate-700">
+        <h1 className="text-4xl text-white">
+          Vite React TypeScript Tailwind Starter
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
